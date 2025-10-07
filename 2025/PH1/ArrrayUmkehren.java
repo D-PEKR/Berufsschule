a099c175d31acff10f9754a7484966c1e748e39e
Peter Kretzer <p.kretzer29@gmail.com>
2025-10-07 09:32:37 +0200

    Array Umkehren

--- 2025/PH1/ArrrayUmkehren.java
+++ 2025/PH1/ArrrayUmkehren.java
@@ -3,8 +3,28 @@ package PH1;
 public class ArrrayUmkehren {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+        double[] original = {1, 2, 3, 4, 5};
+        double[] reversed = reverseArray(original);
 
+        for (double num : reversed) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+    public static double[] reverseArray(double[] array) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            double temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
+        return array;
+    }
 }
